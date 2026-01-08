d2e794a6aeb134306b437b962726db6ce1708f0d
SenKanna43 <senthamaraikannanp@gmail.com>
2026-01-08 18:40:18 +0530

    add RemoveElement class and update main method to demonstrate usage

--- leetcode problems/src/main/java/arrays/Main.java
+++ leetcode problems/src/main/java/arrays/Main.java
@@ -4,12 +4,18 @@ import java.util.Arrays;
 
 public class Main {
         public static void main(String[] args) {
+            // LC 26
             RemoveDuplicates rd = new RemoveDuplicates();
+            int[] nums1 = {0,0,1,1,1,2,2,3,3,4};
+            int k1 = rd.removeDuplicates(nums1);
+            System.out.println("LC26 k = " + k1);
+            System.out.println(Arrays.toString(nums1));
 
-            int[] nums = {0,0,1,1,1,2,2,3,3,4};
-            int k = rd.removeDuplicates(nums);
-
-            System.out.println("k = " + k);
-            System.out.println(Arrays.toString(nums));
+            // LC 27
+            RemoveElement re = new RemoveElement();
+            int[] nums2 = {0,1,2,2,3,0,4,2};
+            int k2 = re.removeElement(nums2, 2);
+            System.out.println("LC27 k = " + k2);
+            System.out.println(Arrays.toString(nums2));
         }
 }
